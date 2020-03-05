599aa4cd4be8334decdc273baacdf03e2841b067
zachwood0s <zach@hayzak.com>
2020-03-05 17:25:30 -0600

    Worked more on the game logic for GOPS

--- common/Games/GOPS.hs
+++ common/Games/GOPS.hs
@@ -38,6 +38,8 @@ data Model = Model
   , _mPlayerHands :: [Hand]
   , _mSelectedCards :: [Maybe Card]
   , _mCardAmount :: Int
+  , _mMainDeck :: Hand
+  , _mMainCard :: Maybe Card
   , _mGameState :: GameState
   } deriving (Show, Eq)
 
@@ -57,6 +59,8 @@ emptyModel cardAmt =
   { _mPlayerTurn = True -- Player 1's Turn 
   , _mPlayerHands = take 2 $ repeat (emptyHand cardAmt)
   , _mSelectedCards = take 2 $ repeat Nothing
+  , _mMainDeck = emptyHand cardAmt
+  , _mMainCard = Nothing
   , _mCardAmount = cardAmt
   , _mGameState = Running
   }
@@ -68,15 +72,42 @@ view :: Model -> Interface action -> View action
 view m iface = 
   div_ 
     []
-    [ div_
+    [ div_ 
+      [ id_ "statusBar" ]
+      [ drawStatus m ]
+    , div_
       [ id_ "gameCanvas" ]
-      (map (flip viewCard iface) [1..m^.mCardAmount])
+      [ viewMainCard (m ^. mMainCard)
+      , div_ 
+        [ id_ "cardBar" ] 
+        (map (flip viewCard iface) playerCards )
+      ]
     ]
+  where 
+    playerCards = currentHand m
+
+viewMainCard :: Maybe Card -> View action
+viewMainCard c = div_ 
+  [ id_ "hiddenCard"
+  , class_ "card"
+  ]
+  [ val c ]
+  where 
+    val (Just v) = text ((toMisoString . show) v)
+    val Nothing = text ""
+
+drawStatus :: Model -> View action
+drawStatus m=
+  h2_ 
+    [ class_ "gameState" ]
+    [ text . toMisoString $ showGameState (m ^. mGameState) (m ^. mPlayerTurn) ]
 
 viewCard :: Card -> Interface action -> View action
 viewCard c iface = 
   a_ 
-    [ onClick (passAction iface $ GOPS $ Move c) ]
+    [ class_ "card"
+    , onClick (passAction iface $ GOPS $ Move c) 
+    ]
     [ text . toMisoString $ show c ]
   
 {----------------
@@ -90,13 +121,25 @@ gops m = Game
   }
 
 update :: Model -> Interface action -> Games.Types.Msg -> GameM action (Game action)
-update m iface DoAI = return $ gops m
+update m iface DoAI = do 
+  return $ gops (newModel m)
+  where
+    newModel m'
+      | m ^. mPlayerTurn = m' 
+        { _mSelectedCards = [] 
+        , _mMainDeck = playCard card ( m^. mMainDeck)
+        , _mMainCard = Just card
+        }
+      | otherwise = m'
+        where 
+          card = (m ^. mMainDeck) !! 0
 update m iface (GOPS msg) = do
   playerTurn <- isPlayerTurn (m ^. mPlayerTurn)
   case msg of 
     Move cardVal | playerTurn -> do 
-      trace "hit" $ gameLog iface (putStrLn "SelectedCard")
-      return $ gops m 
+      gameLog iface (putStrLn "SelectedCard")
+      gameAction iface DoAI
+      return $ gops (fromMaybe m (move cardVal m))
     _ -> return $ gops m
 update m _ _ = return $ gops m
 
@@ -105,35 +148,54 @@ move _ Model{_mGameState = Stalemate} = Nothing
 move _ Model{_mGameState = Won _} = Nothing
 move c m 
   | validMove c =
-      let newHands = map updateHand $ zip [1..] (m ^. mPlayerHands)
+      let newHands = map updateHand $ zip [0..] (m ^. mPlayerHands)
       in 
         Just m 
           { _mPlayerTurn = getNextPlayer (m ^. mPlayerTurn)
           , _mGameState = getGameState newHands 
           , _mPlayerHands = newHands
+          , _mSelectedCards = (m ^. mSelectedCards) ++ [ Just c]
           }
   | otherwise = Nothing
   where 
-    validMove = flip elem ((m ^. mPlayerHands) !! playerIx)
+    validMove = flip elem (currentHand m)
     playerIx = fromEnum (m ^. mPlayerTurn)
     updateHand (ix, hand) = 
       if ix == playerIx then delete c hand
       else hand
 
 updatePlayerHand :: Card -> Model -> Model
-updatePlayerHand c m = 
-  Control.Lens.set (ix playerIx) (playCard c playerHand) playerHand
+updatePlayerHand c m =
+  let
+    newHands = Control.Lens.set (ix playerIx) (playCard c playerHand) hands
+  in
+    m { _mPlayerHands = newHands }
   where 
     playerIx = fromEnum (m ^. mPlayerTurn)
-    playerHand = (m ^. mPlayerHands) !! playerIx
+    hands = m ^. mPlayerHands
+    playerHand = currentHand m
 
+currentHand :: Model -> Hand 
+currentHand m = 
+  (m ^. mPlayerHands) !! playerIx 
+  where 
+    playerIx = fromEnum (m ^. mPlayerTurn)
     
-playCard :: Card -> [Card] -> [Card]
+playCard :: Card -> Hand -> Hand
 playCard = delete 
 
-getGameState :: [[Card]] -> GameState 
+getGameState :: [Hand] -> GameState 
 getGameState ([]:[]:_) = Won True 
 getGameState _ = Running
 
 getNextPlayer :: Player -> Player 
 getNextPlayer = not
+
+showPlayer :: Player -> String
+showPlayer True = "Player 1"
+showPlayer False = "Player 2"
+
+showGameState :: GameState -> Player -> String 
+showGameState (Won x) _= showPlayer x ++ " Won"
+showGameState Stalemate _= "Draw"
+showGameState _ x = showPlayer x ++ "'s Turn"
